cb222010455d0eb9a9683566ce9d2e2a77ba6ece
UCLL <laurent.bubba@hotmail.com>
2025-11-07 12:20:14 +0100

    first push: copied files from docker project, changed workflow file to not use docker

---
+++ app.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { taskRouter } from './controller/task.routes';
+
+const app = express();
+dotenv.config();
+const port = process.env.APP_PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/tasks', taskRouter);
+
+app.get('/status', (req, res) => {
+    res.json({ message: 'Back-end is running...' });
+});
+
+app.listen(port, () => {
+    console.log(`Back-end is running on port ${port}.`);
+});
