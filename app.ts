63f9c68fde6d2aeeff1fcea3151ad6c870d4e036
UCLL <laurent.bubba@hotmail.com>
2025-11-11 14:34:53 +0100

    debugging workflow file

--- app.ts
+++ app.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use('/tasks', taskRouter);
 
 app.get('/status', (req, res) => {
-    res.json({ message: 'Back-end is running... AND CICD IS A BALLERRRRR' });
+    res.json({ message: 'Back-end is running...' });
 });
 
 app.listen(port, () => {
